fdfcb432c6f08b694def4ef6b8ebc1026da26f2f
kumarsaurabh6039 <kumarsaurabh95912@gmail.com>
2025-01-31 12:10:39 +0530

    operator in python

---
+++ operators/operator.py
@@ -0,0 +1,127 @@
+ # Assignment opertor
+# # we assigned 45 as avalue to a variable called num1.
+
+# num1 = 45
+
+# # print(num1)
+
+# # # add and assign
+# num1 += 5
+
+# print(num1)
+
+# # #subtract and assign
+# num1 -= 10
+
+# print(num1)
+
+# # # multiply and assign
+# num1 *= 2
+
+# print(num1)
+
+# # divide and assign
+# num1 /= 4
+
+
+# print(num1)
+
+
+# # floor division and assign
+# num1 //= 3
+
+# print(num1)
+
+# # # exponentiatea nd asign
+# num1 **= 2
+
+# print(num1)
+
+# # # modulus and assign
+# num1 %= 5
+
+# print(num1)
+
+
+# # Comparison Operator
+
+# num1 = 10
+# num2 = 10.0
+
+
+# print(num1 == num2)
+
+# print(23 == 78)
+
+# print(56 == (23 + 33j))
+
+# print(num1 != num2)
+# print(12 != 50)
+
+# print(78 != 78.000001)
+
+# print(num1 > num2)
+
+# print(100 > 50)
+
+
+# print(ord('A') > ord('B'))
+
+# print(num1 < num2)
+
+
+
+# print(num1 >= num2)
+
+# print(34 >= 34)
+
+# print( 45 <= 78)
+
+
+
+# str1 = 'Apple'
+
+# print('p' in str1)
+
+# print('e' in str1)
+
+# fs1 = frozenset({123,  123, 45, 567, 567.00})
+
+# print(len(fs1))
+
+# # print(fs1[0])
+
+# print(567 in fs1)
+
+# print(123 not in fs1)
+
+
+
+# str1 = 'Apple'
+# str2 = 'Mango'
+
+# print(str1 is str2)
+
+# print(str2 is not str1)
+
+
+# num1 = 23
+
+# result = 'Even' if num1 % 2 == 0 else 'Odd'
+
+# print(result)
+
+
+# num2 = 22
+
+# result1 = 'small' if num2 < 20 else 'big'
+
+# print(result1)
+
+
+# fruit = input('Please enter your favourate fruit: ')
+
+# result2 = 'Yellow' if fruit == 'banana' else 'green'
+
+# print(result2)
+
